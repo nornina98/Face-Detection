e52d1c1201e163e4332fb538a3136efd626681cc
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-11-07 00:42:43 +0800

    created components for ImageLink / Rank and also styling css

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f3'>
+                {`THIS MAGIC BRAIN WILL DETECT FACES IN YOUR PICTURES. LET TRY'S IT !`}
+            </p>
+
+            <div className='center'>
+                <div className='form pa4 br3 shadow-5 center'>
+                    <input className='f4 pa2 w-70 center' type='text' placeholder='Please insert your image URL'/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>DETECT</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Rank.css';
+
+
+const Rank = () => {
+    return (
+       <div>
+            <div className='white f3'>
+                {'NorNina, Congratulation Your Rank Is....'}
+            </div>
+            <div className='white f1'>
+                {'#1'}
+            </div>
+       </div>
+    );
+}
+
+export default Rank;
