4122f3cbc1c3cc03b9ce9787bc5c49f5e53f4b64
nornina98 <56819374+nornina98@users.noreply.github.com>
2019-11-09 15:40:54 +0800

    added box detection censored

--- src/App.js
+++ src/App.js
@@ -30,7 +30,27 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol : clarifaiFace.left_col * width,
+      topRow: clarifaiFace.left_col * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.right_col * height)
+    }
+    
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box : box});
   }
 
   onInputChange = (event) => {
@@ -42,17 +62,9 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-      // do something with response
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);     
-    },
-    function(err) {
-      // there was an error
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
     }
-  );
-  
-  }
 
   render() {
     return (
@@ -62,7 +74,7 @@ class App extends Component {
         <Logo /> 
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
       </div>
     )
   }
