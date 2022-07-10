ea43f08fed6e474a2cd1afd52837a27e2fffb207
IKRAM-LAPTOP <nornina8998@gmail.com>
2022-07-11 02:52:37 +0800

    update package.json and make clean line of code (prettier)

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import './ImageLinkForm.css';
+import React from "react";
+import "./ImageLinkForm.css";
 
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className="f3">
+        {`THIS MAGIC BRAIN WILL DETECT FACES IN YOUR PICTURES. LET TRY'S IT !`}
+      </p>
 
-const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
-    return (
-        <div>
-            <p className='f3'>
-                {`THIS MAGIC BRAIN WILL DETECT FACES IN YOUR PICTURES. LET TRY'S IT !`}
-            </p>
-
-            <div className='center'>
-                <div className='form pa4 br3 shadow-5 center'>
-                    <input className='f4 pa2 w-70 center' onChange={onInputChange} type='text' placeholder='Please insert your image URL'/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>DETECT</button>
-                </div>
-            </div>
+      <div className="center">
+        <div className="form pa4 br3 shadow-5 center">
+          <input
+            className="f4 pa2 w-70 center"
+            onChange={onInputChange}
+            type="text"
+            placeholder="Please insert your image URL"
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
+            DETECT
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './Rank.css';
-
+import React from "react";
+import "./Rank.css";
 
 const Rank = () => {
-    return (
-       <div>
-            <div className='white f3'>
-                {'NorNina, Congratulation Your Rank Is....'}
-            </div>
-            <div className='white f1'>
-                {'#1'}
-            </div>
-       </div>
-    );
-}
+  return (
+    <div>
+      <div className="white f3">
+        {"NorNina, Congratulation Your Rank Is...."}
+      </div>
+      <div className="white f1">{"#1"}</div>
+    </div>
+  );
+};
 
 export default Rank;
